ca3721498df1251f8637ed5e355d54e429e907be
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-28 15:35:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   def self.artist_count
     artist_hash = Hash.new
     
-    @@artists.each do |artist|
+    @@artists.each! do |artist|
       if artist_hash[artist]
         artist_hash[artist] += 1
       else
