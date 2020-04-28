0c67ab421469a2fce1eeed75596470f1c87f7bcb
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-28 15:24:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  
+  attr_accessor :name :artist :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count =+ 1
+    if !@@genres.include?(genre)
+      @@genres << genre
+    end
+    
+    if !@@artists.include?(artist)
+      @@artists << artist
+    end
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+   @@genres
+  end
+  
+  def self.artist_count
+    @@artists.count
+  end
+  
+  def self.genre_count
+    @@genres.count
+  end
+end
